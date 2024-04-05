06d43e3190fbb8648129eed6a5c2ff8b677290a5
khalilos34 <khalil.naoueji@gmail.com>
2024-04-05 22:25:49 +0200

    minor changes

--- components/shared/propertyDetails/PropertyInfo.tsx
+++ components/shared/propertyDetails/PropertyInfo.tsx
@@ -64,10 +64,6 @@ const PropertyInfo = ({ property }: { property: IProperty }) => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-lg shadow-md mt-6 w-full">
-        <h3 className="text-lg font-bold mb-6">Images</h3>
-        <PropertyImages images={property.images} />
-      </div>
 
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <h3 className="text-lg font-bold mb-6">Description & Details</h3>
@@ -101,6 +97,10 @@ const PropertyInfo = ({ property }: { property: IProperty }) => {
             </li>
           ))}
         </ul>
+      </div>
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6 w-full">
+        <h3 className="text-lg font-bold mb-6">Images</h3>
+        <PropertyImages images={property.images} />
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <div id="map"></div>
